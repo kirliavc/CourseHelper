da691860ae928fd0ec3b427eae9821c75629be06
Kirlia-Vc <jlc@pku.edu.cn>
2017-02-25 09:57:21 +0800

    选课助手

--- app/src/main/java/com/netlab/vc/coursehelper/WelcomeActivity.java
+++ app/src/main/java/com/netlab/vc/coursehelper/WelcomeActivity.java
@@ -1,8 +1,13 @@
 package com.netlab.vc.coursehelper;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.netlab.vc.coursehelper.util.DeanDecode;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -11,6 +16,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        //new GetPictureTask().execute();
         Intent intent=new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
@@ -19,5 +25,21 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onResume();
         Intent intent=new Intent(this,LoginActivity.class);
         startActivity(intent);
+    }
+    private class GetPictureTask extends AsyncTask<Void,Void,Boolean>{
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                //InputStream is= WebConnection.connect("http://222.29.98.104:8000/ct2.jpg");
+                //Bitmap bm= BitmapFactory.decodeStream(is);
+                //BitmapDrawable bd = new BitmapDrawable(bm);
+                Drawable d = getResources().getDrawable(R.drawable.ct2);
+                Log.e("result", DeanDecode.decode(d));
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
     }
 }
